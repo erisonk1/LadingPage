d0430f19a27465df0b9bdcb126808e30e764eb82
erisonk1 <103046444+erisonk1@users.noreply.github.com>
2023-05-08 11:44:33 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const imgBeneficios = document.querySelectorAll(".beneficios");
+const container = document.querySelectorAll(".container-base")
 let idx = 0
 
 setInterval(() => {
-  carrossel();
-},6000)
-  function carrossel() {
+   function carrossel() {
     idx++;
     if(idx > imgBeneficios.length - 1){
       idx = 0;
     }
-    
     imgBeneficios.forEach((section) => {
       section.classList.remove('ativo');
     })
 
     imgBeneficios[idx].classList.add('ativo');
-
+ 
   }
+  carrossel();
+
+
+},9000);
