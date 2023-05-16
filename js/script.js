51c10da2b4d20750793449f63a81cb38feddba20
Erison Kauã <erisonkauamkt@gmail.com>
2023-05-16 10:18:59 -0300

    s

--- js/script.js
+++ js/script.js
@@ -19,3 +19,45 @@ setInterval(() => {
 
 
 },9000);
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+    
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+initScrollSuave()
+
+const imgDash = document.querySelectorAll('.img-dash')
+
+let idx2 = 0;
+setInterval(() => {
+  function carrossel2() {
+   idx2++;
+   if(idx2 > imgDash.length - 1){
+     idx2 = 0;
+   }
+   imgDash.forEach((section) => {
+     section.classList.remove('ativo');
+   })
+
+   imgDash[idx2].classList.add('ativo');
+
+ }
+ carrossel2();
+
+
+},7000);
